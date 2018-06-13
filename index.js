9028b79127bb82e098d0a91d9f2e38e334078767
zhangfu <zhangfu@nasetech.com>
2018-06-13 22:34:39 +0800

    update

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ function select(val) {
   return document.querySelector(val);
 }
 
-// 左边手柄
-var leftJoystick = new Joystick({
+new Joystick({
   zone: select('#left')
-});
-
-var leftJoystickManager = leftJoystick.init();
-
-leftJoystickManager.onStart = function(distance, angle) {
+})
+.init()
+.onStart = function(distance, angle) {
   // console.log('Left:无人机向 => ' + angle + '移动' + distance + '个单位');
-  
   switch(angle) {
     case 'up':
       Game.jump(distance);
@@ -22,8 +18,4 @@ leftJoystickManager.onStart = function(distance, angle) {
     case 'left':
       Game.backward(distance);
   }
-}
-
-leftJoystickManager.onEnd = function() {
-  Game.stop();
 }
