ff1d4e964737d570a989dcade9b3831e3429d271
zhangfu <zhangfu@nasetech.com>
2018-06-13 21:42:10 +0800

    rm node_module

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var leftJoystickManager = leftJoystick.init();
 
 leftJoystickManager.onStart = function(distance, angle) {
   // console.log('Left:无人机向 => ' + angle + '移动' + distance + '个单位');
+  
   switch(angle) {
     case 'up':
       Game.jump(distance);
@@ -26,14 +27,3 @@ leftJoystickManager.onStart = function(distance, angle) {
 leftJoystickManager.onEnd = function() {
   Game.stop();
 }
-
-// // 右边手柄
-// var rightJoystick = new Joystick({
-//   zone: select('#right')
-// });
-
-// var rightJoystickManager = rightJoystick.init();
-
-// rightJoystickManager.onStart = function(distance, angle) {
-//   console.log('Right:无人机向 => ' + angle + '移动' + distance + '个单位');
-// }
